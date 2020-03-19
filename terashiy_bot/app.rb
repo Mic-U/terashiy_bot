a0d8ff7f203e1f5c8f40fb3c7b7dcccd989d550a
Mic-U <yuho.miki@gmail.com>
2020-03-20 00:15:32 +0900

    Modify template.

--- terashiy_bot/app.rb
+++ terashiy_bot/app.rb
@@ -5,21 +5,20 @@ require 'time'
 require_relative './blog_content'
 require_relative './ddb'
 
-@blog_url = 'http://blog.livedoor.jp/terashimatakuma/'
-@table_name = ENV['DYNAMO_TABLE']
-
 def lambda_handler(event:, context:)
-  post = latest_post(blog_url: @blog_url)
+  blog_url = 'http://blog.livedoor.jp/terashimatakuma/'
+  table_name = ENV['DYNAMO_TABLE']
+  post = latest_post(blog_url: blog_url)
 
   ddb = DDBConnector.new
-  latest_date = ddb.latest_date(table_name: @table_name, id: @blog_url)
+  latest_date = ddb.latest_date(table_name: table_name, id: blog_url)
 
   unless newer_than_latest_date?(post_date: post.date, latest_date: latest_date)
     return
   end
 
   # TODO: Notify function
-  ddb.update_date(table_name: @table_name, id: @blog_url, new_date: post.date)
+  ddb.update_date(table_name: table_name, id: blog_url, new_date: post.date)
 end
 
 def newer_than_latest_date?(post_date:, latest_date:)
