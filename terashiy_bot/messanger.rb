04bee8700af9d5b773f696f462f7f9c7e1f7637f
Mic-U <yuho.miki@gmail.com>
2020-03-20 09:09:51 +0900

    line notification

---
+++ terashiy_bot/messanger.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'httparty'
+
+# Send message to LINE channel
+class LineMessanger
+  def initialize(token:)
+    @line_url = 'https://api.line.me/v2/bot/message/broadcast'
+    @token = token
+  end
+
+  def send(blog_title:, url:)
+    messages = convert_message(blog_title: blog_title, url: url)
+    HTTParty.post(@line_url,
+                  body: messages.to_json,
+                  headers: {
+                    'Content-Type' => 'application/json',
+                    'Authorization' => "Bearer #{@token}"
+                  })
+  end
+
+  def convert_message(blog_title:, url:)
+    {
+      messages: [
+        {
+          type: 'text',
+          text: "【新着】 #{blog_title} \n #{url}"
+        }
+      ]
+    }
+  end
+end
