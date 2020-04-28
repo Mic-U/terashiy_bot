0b517d6c4451b401d26772ce7b031619fa57018c
Mic-U <yuho.miki@gmail.com>
2020-04-29 07:15:33 +0900

    Refactor

--- terashiy_bot/app.rb
+++ terashiy_bot/app.rb
@@ -8,27 +8,26 @@ require_relative './blog_content'
 require_relative './ddb'
 require_relative './messanger'
 
-require 'aws-xray-sdk/lambda'
-
 Dotenv.load! "#{__dir__}/.env"
 
 def lambda_handler(event:, context:)
+  require 'aws-xray-sdk/lambda'
   blog_url = 'http://blog.livedoor.jp/terashimatakuma/'
-  table_name = ENV['DYNAMO_TABLE']
   post = latest_post(blog_url: blog_url)
   puts "post: #{post}"
 
-  ddb = DDBConnector.new
-  latest_date = ddb.latest_date(table_name: table_name, id: blog_url)
-
-  puts "latest_date #{latest_date}"
-  if older_than_latest_date?(post_date: post[:date], latest_date: latest_date)
-    return
-  end
+  ddb = DDBConnector.new(table_name: ENV['DYNAMO_TABLE'])
+  return if new_post?(ddb: ddb, id: blog_url, post_date: post[:date])
 
   token = ENV['LINE_ACCESS_TOKEN']
   send_message(blog_title: post[:title], url: blog_url, token: token)
-  ddb.update_date(table_name: table_name, id: blog_url, new_date: post[:date])
+  ddb.update_date(id: blog_url, new_date: post[:date])
+end
+
+def new_post?(ddb:, id:, post_date:)
+  latest_date = ddb.latest_date(id: blog_url)
+  puts "latest_date #{latest_date}"
+  older_than_latest_date?(post_date: post_date, latest_date: latest_date)
 end
 
 def older_than_latest_date?(post_date:, latest_date:)
